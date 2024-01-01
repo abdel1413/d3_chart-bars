7453cd1ff35f9fbebd12afe15b4ede8bc6d3ae7a
Your Name <you@example.com>
2024-01-01 15:50:19 -0500

    added circleds

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 d3.select("ul").append("li").text("This is an important Item");
+d3.select("body")
+  .append("svg")
+  .attr("whidth", 400)
+  .attr("height", 400)
+  .append("circle")
+  .attr("cx", 100)
+  .attr("cy", 100)
+  .attr("r", 50)
+  .style("fill", "green");
+
+let bodySelector = d3.select("body");
+console.log(bodySelector);
+let svgSelector = bodySelector
+  .append("svg")
+  .attr("width", 300)
+  .attr("height", 300);
+let circleSelector = svgSelector
+  .append("circle")
+  .attr("cx", 90)
+  .attr("cy", 90)
+  .attr("r", 50)
+  .style("fill", "black");
