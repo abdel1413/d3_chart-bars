249fe84274039250264eae9c75ed6712fa4b9bb3
Your Name <you@example.com>
2024-01-02 13:46:56 -0500

    dynamically display circles based on the data set

---
+++ dynamicRec.js
@@ -0,0 +1,24 @@
+const dataset = [30, 70, 110];
+
+const b = d3
+  .select("body")
+  .append("svg")
+  .attr("width", 300)
+  .attr("height", 300);
+console.log(body);
+
+const cir = b.selectAll("circle").data(dataset).enter().append("circle");
+
+const drw = cir
+  .attr("cx", (d) => d)
+  .attr("cy", (d) => d)
+  .attr("r", 20)
+  .style("fill", (d) => {
+    let color;
+    d == 30
+      ? (color = "blue")
+      : d == 70
+      ? (color = "purple")
+      : (color = "yellow");
+    return color;
+  });
